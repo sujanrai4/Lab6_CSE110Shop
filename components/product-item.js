567b7f624b800abfdc2d09bd362c91c3528a3e35
sujanrai4 <sjn.bvet5@yahoo.com>
2021-02-12 10:48:58 -0800

    finalized lab6

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,110 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor() {
+    super();
+    // attach a shadow root to a custome element
+    let shadowRoot = this.attachShadow({mode: 'open'});
+    // create product items as LI tags
+    let liElement = document.createElement('li');
+    liElement.setAttribute('class', 'product');
+
+    // create img element
+    let imgElement = document.createElement('img');
+    imgElement.setAttribute('width', '200');
+    imgElement.src = this.getAttribute('src');
+    imgElement.alt = this.getAttribute('alt');
+    liElement.appendChild(imgElement);
+
+    // create two p elements as children of liElement
+    let p1Element = document.createElement('p');
+    p1Element.setAttribute('class', 'title');
+    liElement.appendChild(p1Element);
+    
+    let p2Element = document.createElement('p');
+    p2Element.setAttribute('class', 'price');
+    liElement.appendChild(p2Element);
+
+    // create a button element
+    let buttonElement = document.createElement('button');
+    buttonElement.textContent = 'Add to Cart';
+    // make onclick event to respond the function updateCart
+    buttonElement.setAttribute('onclick', 'updateCart(this)');
+    liElement.appendChild(buttonElement);
+
+    // make liElement as a child of shadowRoot
+    shadowRoot.appendChild(liElement);
+
+    // create style element
+    let shadowStyle = document.createElement('style');
+    shadowStyle.textContent = `
+      .price {
+        color: green;
+        font-size: 1.8em;
+        font-weight: bold;
+        margin: 0;
+      }
+      
+      .product {
+        align-items: center;
+        background-color: white;
+        border-radius: 5px;
+        display: grid;
+        grid-template-areas: 
+        'image'
+        'title'
+        'price'
+        'add';
+        grid-template-rows: 67% 11% 11% 11%;
+        height: 450px;
+        filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+        margin: 0 30px 30px 0;
+        padding: 10px 20px;
+        width: 200px;
+      }
+      
+      .product > button {
+        background-color: rgb(255, 208, 0);
+        border: none;
+        border-radius: 5px;
+        color: black;
+        justify-self: center;
+        max-height: 35px;
+        padding: 8px 20px;
+        transition: 0.1s ease all;
+      }
+      
+      .product > button:hover {
+        background-color: rgb(255, 166, 0);
+        cursor: pointer;
+        transition: 0.1s ease all;
+      }
+      
+      .product > img {
+        align-self: center;
+        justify-self: center;
+        width: 100%;
+      }
+      
+      .title {
+        font-size: 1.1em;
+        margin: 0;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+      
+      .title:hover {
+        font-size: 1.1em;
+        margin: 0;
+        white-space: wrap;
+        overflow: auto;
+        text-overflow: unset;
+      }`;
+  // apply shadowStyle to shadowRoot
+  shadowRoot.appendChild(shadowStyle);
+  }
+
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,77 @@
-// Script.js
+/**
+ * script.js
+ * References: https://www.w3schools.com/jsref/jsref_from.asp
+ *             https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/String
+ *             https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+ *              
+ *  */ Script.js
 
+// create a Set of products(objects), cart, that will update the items in the shopping Cart
+var cart = new Set(); 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  console.log('Hello');
+  // create a Set of products(objects), cart, that will update the items in the shopping Cart
+  if(localStorage.getItem('itemArr') == null) {
+    // first time fetching the JSon data file
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => localStorage.setItem('itemArr', JSON.stringify(data)));
+      //loop through the data and add each item as children of shadowRoot
+      let count = 0;
+      let itemArrString = JSON.stringify(localStorage.getItem('itemArr'));
+      for(count; count < itemArrString.length; count++) {
+        let item = itemArrString[count];
+        let product = document.createElement('product-item');
+        product.shadowRoot.children[0].children[0].src = item.image;
+        product.shadowRoot.children[0].children[0].alt = item.title;
+        product.shadowRoot.children[0].children[1].innerHTML = item.title;
+        product.shadowRoot.children[0].children[2].innerHTML = '$' + item.price;
+
+        // assign an id to check if the item is in the cart later on
+        product.shadowRoot.children[0].children[3].setAttribute('id', item.id);
+      }
+  } else {
+    // load the cart from localStorage to update the shopping cart
+    cart = new Set(JSON.parse(localStorage.getItem('cart')));
+    // update the cart count
+    document.getElementById('cart-count').innerHTML = cart.size;
+    // load product array from the localStorage into new string array
+    let stringArray = JSON.parse(localStorage.getItem('itemArr'));
+    let count = 0;
+    console.log('Hello');
+    for(count; count < stringArray.length; count++) {
+      const item = stringArray[count];
+      let product = document.createElement('product-item');
+      console.log('Hello');
+      product.shadowRoot.children[0].children[0].src = item.image;
+      product.shadowRoot.children[0].children[0].alt = item.title;
+      product.shadowRoot.children[0].children[1].innerHTML = item.title;
+      product.shadowRoot.children[0].children[2].innerHTML = '$' + item.price;
+
+      // assign an id to check if the item is in the cart later on
+      product.shadowRoot.children[0].children[3].setAttribute('id', item.id);
+      // check if the item is already in the shopping cart. if it is set button to read removed from cart
+      if(cart.has(String(item.id))) {
+        product.shadowRoot.children[0].children[3].innerHTML = 'Remove from Cart';
+      }
+      document.getElementById('product-list').appendChild(product);
+    } 
+  }
+
+
+    
 });
+function updateCart(event) {
+  if(cart.has(event.id)) {
+    cart.delete(event.id);
+    event.innerHTML = 'Add to Cart';
+  } else {
+    cart.add(event.id);
+    event.innerHTML = 'Remove from Cart';
+  }
+  // update the cart count after each load
+  document.getElementById('cart-count').innerHTML = cart.size;
+  // update localStorage. Array.from() will convert iterable cart into 
+  //array object and stringify will convert into JSON strings
+  localStorage.setItem('cart', JSON.stringify(Array.from(cart)));
+}
